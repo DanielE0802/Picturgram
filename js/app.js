b520014cf4102ec9dd89fd7e20ab8a4211b3e82e
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-04-04 14:59:05 -0500

    update api

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 const url = `https://api.unsplash.com/photos/?client_id=7tlK20S_-PXvoucU1TQi_CrGfgGhhpoHHDlrlqLIWTA`
 
 let containerImg = document.querySelectorAll("body > main > div > div.main-left > div.main-left-post > div > div.post-img > img")
+let autores = document.querySelectorAll("#main > div.main-left > div.main-left-post > div > div.user > p")
+let perfilImg = document.querySelectorAll("#main > div.main-left > div.main-left-post > div > div.user > img")
+let description = document.querySelectorAll("#main > div.main-left > div.main-left-post > div > div.reactions > div.description")
+let date = document.querySelectorAll("#main > div.main-left > div.main-left-post > div > div.reactions > div.date")
 
-
-callApi = async  () => {
-    let data = await fetch (url)
+callApi = async () => {
+    let data = await fetch(url)
     let resp = await data.json()
     return resp
 }
@@ -14,8 +17,11 @@ let info = callApi()
 info.then(response => {
     for (var i = 0; i <= 10; i++) {
         containerImg[i].setAttribute("src", response[i].urls.regular)
+        autores[i].innerHTML= "@" + response[i].user.instagram_username
+        perfilImg[i].setAttribute("src", response[i].user.profile_image.medium )
+        description[i].innerHTML = response[i].alt_description
+        date[i].innerHTML = response[i].created_at
     }
 }).catch(error => {
     console.error(error);
 })
-
--- js/responsive.js
+++ js/responsive.js
@@ -1,12 +1,51 @@
 
+
+let main = document.getElementById('main')
+let inicio = document.getElementById('iniciar')
+let entrar = document.getElementById('entrar')
+let header = document.getElementById('header')
+let logo = document.getElementById('logo')
+let bar = document.getElementById('bar')
+
+entrar.addEventListener('click', function(){
+    inicio.classList.add('display-none')
+    main.classList.remove('display-none')
+    header.classList.remove('display-none')
+
+    
+}
+)
+
+
+
+
+
+
 setInterval(() => {
-    let bar = document.getElementById('bar')
-    if (screen.width > 700) {
+    let valorInicio = inicio.className
+    if (screen.width > 700 ) {
         bar.classList.add('display-none')
     }
     else{
-        bar.classList.remove('display-none')
+        if( valorInicio !== "main-inicio display-none"){
+            bar.classList.add('display-none')
+           
+        }
+        else{
+            bar.classList.remove('display-none')
+        }
     }
 
 
-}, 500);
+}, 200);
+
+
+function mainAInicio(){
+ 
+}
+
+logo.addEventListener('click',function(){
+    inicio.classList.remove('display-none')
+    main.classList.add('display-none')
+    header.classList.add('display-none')
+})
